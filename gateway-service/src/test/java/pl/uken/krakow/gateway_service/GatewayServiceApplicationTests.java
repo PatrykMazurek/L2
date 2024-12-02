e4d93b49840f7ad21a51bb7c7030cbe1c3f3bf5e
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2024-12-02 13:19:59 +0100

    Plki do zajęć

---
+++ gateway-service/src/test/java/pl/uken/krakow/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uken.krakow.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web-service/src/main/java/pl/uken/web_service/model/Members.java
@@ -0,0 +1,18 @@
+package pl.uken.web_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Members {
+    
+    String club;
+    String function;
+    int id;
+    String lastFirstName;
+}
